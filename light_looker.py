21ed6c1ff7dbbf1ceb01d332fda0926a949612a2
FRC 2399 TJ <jallen@hb.edu>
2014-02-09 23:37:20 +0000

    Added hot or not capabilities

--- light_looker.py
+++ light_looker.py
@@ -17,6 +17,8 @@ except ImportError:
 
 
 # Enable graphical debug mode, including viewing windows
+# For testing make DEBUG = True and ROBOT = False
+# For actual use make DEBUG = False and ROBOT = True
 DEBUG = True
 ROBOT = False
 
@@ -60,29 +62,32 @@ def get_cap():
     
     return cap
 
-
+#This method skips frames on the camera
 def skip_frames(cap, n):
     'Skips n frames on a VideoCapture device'
     for i in range(n):
         cap.read()
 
-
+#This method sets the LEDs on or off
 def set_leds(x):
     'Turns LEDs on or off (True is on, False is off)'
     for led in [RED_LED, GREEN_LED, BLUE_LED]:
 
         GPIO.output(led, x)
-
+        
+#This method provides us with a smooth shutdown via a joystick button
 def shutdown():
     if USE_GPIO:
         set_leds(True)
     os.system("sudo shutdown -h now")
     exit(0)
 
+#This method sets up the aspect ratios of a rectangle
 def aspect_ratio(rectangle):
     x, y, w, h = rectangle
     return w/h
 
+#This method tells us if a rectangle is a static target or not
 def is_static_target(rect):
     ar = aspect_ratio(rect)
     if ar <= 0.2 and ar >= 0.15:
@@ -90,6 +95,7 @@ def is_static_target(rect):
     else:
         return False
 
+#This method tells us if a rectangle is a dynamic rectangle or not
 def is_dynamic_target(rect):
     ar = aspect_ratio(rect)
     if ar <= 5.45 and ar >= 4.5:
@@ -97,14 +103,19 @@ def is_dynamic_target(rect):
     else:
         return False
 
+#This method tells us the area of a rectangle 
 def area(rect):
-    x, y, w, h = rectangle
+    x, y, w, h = rect
     return w*h
 
+#This method tells us if the camera has detected the static and dynamic targets
+#Also if their ratios are within the min and max range
 def are_target_pair(rect1, rect2):
-    #TODO: Write this!!!
-    #ratio = 
-    #if is_static_target(rect1) and is_dynamic_target(rect2) and
+    ratio = area(rect1) / area(rect2)
+    if is_static_target(rect1) and is_dynamic_target(rect2) and ratio >= 1.4 and ratio <= 1.75:
+        return True
+    else:
+        return False
 
 def main():
     if USE_GPIO:
@@ -114,7 +125,8 @@ def main():
         GPIO.setup(RED_LED, GPIO.OUT)
         GPIO.setup(GREEN_LED, GPIO.OUT)
         GPIO.setup(BLUE_LED, GPIO.OUT)
-    
+
+    #Self explanatory but this sets the exposure to the level we want it 
     set_exposure(EXPOSURE_LEVEL)
 
     if DEBUG:
@@ -124,7 +136,8 @@ def main():
         cv2.namedWindow('d', cv2.WINDOW_AUTOSIZE)
 
     cap = get_cap()
-    
+
+    #This connectes the pi to the Netwrok Tables
     if ROBOT:
         client = nt_client.NetworkTableClient("2399")
 
@@ -203,27 +216,80 @@ def main():
         contours, hierarchy = cv2.findContours(contimg, 1, 2)
         contours.sort(key = cv2.contourArea, reverse = True)
 
+        #This tells us if we've found a pair of rectangles\
+        found_pair = False
+        found_static = False
         if len(contours) >= 2:
-             
-            cnt1 = contours[0]
-            cnt2 = contours[1]
-            rect = cv2.boundingRect(cnt1)
+            for i in range(len(contours)):
+                cnt1 = contours[i]
+                for j in range(i + 1, len(contours)):
+                    cnt2 = contours[j]
+                    if are_target_pair(cv2.boundingRect(cnt1), cv2.boundingRect(cnt2)):
             
-            x, y, w1, h1 = cv2.boundingRect(cnt1)
-            cv2.rectangle(contimg, (x,y), (x+w1, y+h1), 200)
-            x, y, w2, h2 = cv2.boundingRect(cnt2)
-            cv2.rectangle(contimg, (x,y), (x+w2, y+h2), 200)
-            area1 = cv2.contourArea(cnt1)
-            area2 = cv2.contourArea(cnt2)
+                        x, y, w1, h1 = cv2.boundingRect(cnt1)
+                        cv2.rectangle(contimg, (x,y), (x+w1, y+h1), 200)
+                        x, y, w2, h2 = cv2.boundingRect(cnt2)
+                        cv2.rectangle(contimg, (x,y), (x+w2, y+h2), 200)
+                        dist = 20726 * h1 **(-1.138)
 
-            dist = 20726 * h1 **(-1.138)
+                        if ROBOT:
+                            
+                            client.setValue("/Vision/distance", dist)
+                            
+                            
+                        print dist
 
+                        found_pair = True
+                        print "hot"
+                        if ROBOT:
+                            client.setValue("/Vision/hot", True)
+                        break
+                    
+                if found_pair:
+                    break
+        if not found_pair:       
+            print "not"
             if ROBOT:
+                client.setValue("/Vision/hot", False)
                 
-                client.setValue("/Vision/distance", dist)
+        if not found_pair and len(contours) >= 1:
+            for i in range(len(contours)):
+                cnt1 = contours[i]
+                if is_static_target(cv2.boundingRect(cnt1)):
+                    x, y, w1, h1 = cv2.boundingRect(cnt1)
+                    cv2.rectangle(contimg, (x,y), (x+w1, y+h1), 200)
+                    dist = 20726 * h1 **(-1.138)
+
+                    if ROBOT:
+                            
+                        client.setValue("/Vision/distance", dist)
+                            
+                            
+                    print dist
+                    found_static = True
+                    break
                 
-                
-            print dist 
+
+            #This finds rectangle contours
+##            cnt1 = contours[0]
+##            cnt2 = contours[1]
+##            rect = cv2.boundingRect(cnt1)
+##            
+##            x, y, w1, h1 = cv2.boundingRect(cnt1)
+##            cv2.rectangle(contimg, (x,y), (x+w1, y+h1), 200)
+##            x, y, w2, h2 = cv2.boundingRect(cnt2)
+##            cv2.rectangle(contimg, (x,y), (x+w2, y+h2), 200)
+##            area1 = cv2.contourArea(cnt1)
+##            area2 = cv2.contourArea(cnt2)
+##
+##            dist = 20726 * h1 **(-1.138)
+##
+##            if ROBOT:
+##                
+##                client.setValue("/Vision/distance", dist)
+##                
+##                
+##            print dist 
         #print w1, h1, w2, h2
 
         #M = cv2.moments(cnt1)
